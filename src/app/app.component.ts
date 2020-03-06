9afd17cee72bc307e091de5259aff115d395b6a4
Francois Weber <francois.weber@tangentsolutions.co.za>
2020-03-06 17:06:28 +0200

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import * as jsPDF from 'jspdf'
+import { Chart } from 'angular-highcharts';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  graphComplete = false;
+
+  pdfSafe: SafeResourceUrl;
   title = 'print-test';
+  chart = new Chart({
+    exporting: {
+      fallbackToExportServer: false
+    },
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Line 1',
+        data: [1, 2, 3],
+        type: 'line'
+      }
+    ]
+  });
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+    this.graphComplete = false;
+    let doc = new jsPDF('p', 'cm', 'a4');
+    doc.text(1, 1, 'some text');
+    doc.setProperties({
+      title: 'myPdf'
+    });
+    this.chart.ref$.pipe(
+      filter(ref => ref != null && !this.graphComplete),
+    ).subscribe(ref => {
+      this.graphComplete = true;
+      const svg = ref['getSVG']();
+      console.log(svg);
+      // const datauri = doc.output('datauristring', {
+      //   filename: 'myPdf'
+      // });
+      // this.pdfSafe = this.sanitizer.bypassSecurityTrustResourceUrl(datauri);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import * as more from 'highcharts/highcharts-more.src';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as exporting from 'highcharts/modules/exporting.src';
+import * as offlineExporting from 'highcharts/modules/offline-exporting.src';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [ more, exporting, offlineExporting ] }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
